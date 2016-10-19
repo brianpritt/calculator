a6a01f8ee16279821a1ef73b2f7f4b90a3cfe9bf
Anne Belka and Brian Pritt <anne@>
2016-10-19 13:41:12 -0700

    adds form functionality to calculator application

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,39 @@
-function add(number1, number2){
-	return number1+ number2;
+var add = function(number1, number2) {
+  return number1 + number2;
 };
 
-alert(add(10,5));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function(){
+	$("form#add").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+	$("form#subtract").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#sub1").val());
+		var number2 = parseInt($("#sub2").val());
+		var result = subtract(number1, number2);
+		$("#output").text(result);
+	});
+	$("form#multiply").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#mult1").val());
+		var number2 = parseInt($("#mult2").val());
+		var result = subtract(number1, number2);
+		$("#output").text(result);
+	});
+});
